1229cfeffdaeb4b61cc50b466ad0584eb9e0f9c2
akita <zeal23310029@fsgcl.jp>
2024-06-17 12:17:27 +0900

    練習問題10

--- src/PlayerBullet.java
+++ src/PlayerBullet.java
@@ -1,5 +1,10 @@
 
 public class PlayerBullet extends Character{
+	public void draw(MyFrame f){
+		f.setColor(0, 128,128);
+		f.fillRect(x+10, y-10 , 10, 10);
+		
+	}
 
 	public PlayerBullet(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
