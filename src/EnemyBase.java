7f82df1ad448c2c0000d19abf402b8e85b87d184
akita <zeal23310029@fsgcl.jp>
2024-06-24 11:12:35 +0900

    12.5　エネベース耐久力高く

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -4,6 +4,7 @@ public class EnemyBase extends Enemy {
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x, y, vx, vy);
 		// TODO 自動生成されたコンストラクター・スタブ
+		life=20;
 	}
 	public void draw(MyFrame f) {
 		 f.setColor(0, 128, 0); 
