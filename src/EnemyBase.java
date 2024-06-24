97ab8cc838574bce0653662198d4c7f117dc12d3
akita <zeal23310029@fsgcl.jp>
2024-06-24 10:09:03 +0900

    12.1当たり判定：自分

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -16,16 +16,16 @@ public class EnemyBase extends Enemy {
 		super.move();
 		if(x>300)vx=-1;
 		if(x<100)vx=1;
-		if(Math.random()<0.05) {
+		if(Math.random()<0.04) {
 			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
 		}
-		if(Math.random()<0.05) {
+		if(Math.random()<0.02) {
 			GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
 		}
-		if(Math.random()<0.05) {
+		if(Math.random()<0.03) {
 			GameWorld.enemies.add(new DropEnemy(x,y,0,4));
 		}
-		if(Math.random()<0.05) {
+		if(Math.random()<0.03) {
 			GameWorld.enemies.add(new CurveEnemy(x,y,0,1));
 		}
 	}
